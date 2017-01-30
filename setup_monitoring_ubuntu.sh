b9b40e8db6fb54d0c7e90f080a778f37f9ba2851
root <root@gw08n06.ffs.ovh>
2017-01-30 22:41:34 +0100

    collectd config

--- setup_monitoring_ubuntu.sh
+++ setup_monitoring_ubuntu.sh
@@ -353,3 +353,51 @@ chmod +x /usr/local/bin/check-tasks
 ensureline_insert "/usr/local/bin/check-tasks &" /etc/rc.local
 }
 
+setup_monitoring_collectd() {
+cat <<EOF >/etc/collectd/collectd.conf
+Hostname "$HOSTNAME"
+LoadPlugin syslog
+
+<Plugin syslog>
+    LogLevel info
+</Plugin>
+
+LoadPlugin cpu
+LoadPlugin df
+LoadPlugin disk
+LoadPlugin entropy
+LoadPlugin interface
+LoadPlugin irq
+LoadPlugin load
+LoadPlugin memory
+LoadPlugin processes
+LoadPlugin rrdtool
+LoadPlugin swap
+LoadPlugin uptime
+LoadPlugin write_graphite
+
+<Plugin df>
+    FSType rootfs
+    FSType sysfs
+    FSType proc
+    FSType devtmpfs
+    FSType devpts
+    FSType tmpfs
+    FSType fusectl
+    FSType cgroup
+    IgnoreSelected true
+</Plugin>
+
+<Plugin rrdtool>
+    DataDir "/var/lib/collectd/rrd"
+</Plugin>
+
+<Plugin write_graphite>
+    <Node "netinfo">
+        Host "10.191.255.243"
+        Port "2003"
+        Protocol "tcp"
+    </Node>
+</Plugin>
+EOF
+}
