c08f82223b6492bea7c96379f9e518071ffbc024
albi <albi@seth4.albi.life.de>
2017-02-20 15:08:20 +0100

    Direktausleitung bestimmter Ports

--- setup_openvpn_ubuntu.sh
+++ setup_openvpn_ubuntu.sh
@@ -23,6 +23,18 @@ ip route add 128.0.0.0/1 via \$route_vpn_gateway dev \$dev table stuttgart
 iptables -t nat -A POSTROUTING -o \$dev -j MASQUERADE
 sysctl -w net.netfilter.nf_conntrack_max=500000
 exit 0
+
+# https+Mailports direkt ausleiten
+ip rule add fwmark 0x2000 lookup direct priority 6000
+iptables -t mangle -A PREROUTING -j MARK --set-xmark 0x0/0xffffffff
+iptables -t mangle -A FORWARD -j MARK --set-xmark 0x0/0xffffffff
+for port in $DIRECTTCP; do
+  iptables -t mangle -A PREROUTING -s 10.190.0.0/15 -p tcp -m tcp --dport \$port -j MARK --set-xmark 0x2000/0xffffffff
+  iptables -t mangle -A FORWARD -s 10.190.0.0/15 -p tcp -m tcp --dport \$port -j MARK --set-xmark 0x2000/0xffffffff
+  iptables -t nat -A POSTROUTING -o eth0 -p tcp --dport \$port -j SNAT --to-source $EXT_IP_V4
+done
+ip route show table main | while read ROUTE ; do ip route add table direct \$ROUTE ; done
+exit 0
 EOF
 chmod +x /etc/openvpn/openvpn-up
 cat <<-EOF >/etc/openvpn/openvpn-down
@@ -30,6 +42,16 @@ cat <<-EOF >/etc/openvpn/openvpn-down
 ip rule del from \$ifconfig_local table stuttgart priority 9970
 # NAT deaktivieren, wird benötigt wenn NICHT Berlin
 iptables -t nat -D POSTROUTING -o \$dev -j MASQUERADE
+#exit 0
+
+# https+Mailports direkt ausleiten
+ip rule del fwmark 0x2000 lookup direct priority 6000
+for port in $DIRECTTCP; do
+  iptables -t mangle -D PREROUTING -s 10.190.0.0/15 -p tcp -m tcp --dport \$port -j MARK --set-xmark 0x2000/0xffffffff
+  iptables -t nat -D POSTROUTING -o eth0 -p tcp --dport \$port -j SNAT --to-source $EXT_IP_V4
+done
+iptables -t mangle -D PREROUTING -j MARK --set-xmark 0x0/0xffffffff
+ip route flush table direct
 exit 0
 EOF
 chmod +x /etc/openvpn/openvpn-down
