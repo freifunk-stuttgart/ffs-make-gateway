dda577fbbb3e1583d6da27d95a139d51be44900c
albi <albi@seth4.albi.life.de>
2017-03-09 17:40:06 +0100

    tinc metric eingebaut 1024. tincl3 eingebaut metric 512

--- setup_tinc_ubuntu.sh
+++ setup_tinc_ubuntu.sh
@@ -15,7 +15,6 @@ ConnectTo = gw05n02
 ConnectTo = gw08n03
 Mode = switch
 Port = 6551
-IndirectData = yes
 #GraphDumpFile = /tmp/ffsbb.gv
 EOF
 cat <<EOF >/etc/tinc/ffsbb/tinc-up
@@ -26,9 +25,8 @@ ip rule add iif \$INTERFACE table stuttgart priority 7000
 ip route add 10.191.255.0/24 proto kernel scope link src 10.191.255.$GWID$GWSUBID dev \$INTERFACE table stuttgart
 ip route add 10.190.0.0/15 dev \$INTERFACE
 ip route add 10.190.0.0/15 dev \$INTERFACE table stuttgart
-ip addr add fd21:b4dc:4b00::a38:$GWLID$GWLSUBID/64 dev \$INTERFACE
-ip route add fd21:b4dc:4b00::/40 dev \$INTERFACE
-ip route add fd21:b4dc:4b00::/40 dev \$INTERFACE table stuttgart
+ip addr add fd21:b4dc:4b00::a38:$GWLID$GWLSUBID/40 dev \$INTERFACE metric 1024
+ip route add fd21:b4dc:4b00::/40 dev \$INTERFACE metric 1024 table stuttgart
 service isc-dhcp-server restart
 EOF
 chmod +x /etc/tinc/ffsbb/tinc-up
@@ -61,6 +59,71 @@ setup_tinc_key() {
 if [ ! -d /etc/tinc/ffsbb/hosts.1 ]; then
   mv /etc/tinc/ffsbb/hosts /etc/tinc/ffsbb/hosts.1
   ln -s /root/tinc-ffsbb/hosts /etc/tinc/ffsbb/hosts
+fi
+}
+
+
+setup_tincl3_config() {
+if [ ! -d /root/tinc-ffsl3/.git ]; then
+  git clone https://github.com/freifunk-stuttgart/tinc /root/tinc-ffsl3
+fi
+if [ ! -d /etc/tinc/ffsl3 ]; then
+    mkdir -p /etc/tinc/ffsl3/hosts
+fi
+cat <<EOF >/etc/tinc/ffsl3/tinc.conf
+Name = $HOSTNAME
+ConnectTo = dhcp01
+ConnectTo = dhcp02
+ConnectTo = dhcp03
+ConnectTo = gw01n03
+ConnectTo = gw04n01
+ConnectTo = gw05n02
+ConnectTo = gw08n05
+Mode = router
+Port = 6552
+#GraphDumpFile = /tmp/ffsl3.gv
+EOF
+cat <<EOF >/etc/tinc/ffsl3/tinc-up
+#!/bin/sh
+ip addr add 10.191.254.$GWID$GWSUBID/24 broadcast 10.191.255.255 dev \$INTERFACE
+ip link set \$INTERFACE up
+ip rule add iif \$INTERFACE table stuttgart priority 7000
+ip route add 10.191.254.0/24 dev $INTERFACE table stuttgart
+
+ip route add 10.191.255.0/24 proto kernel scope link src 10.191.255.$GWID$GWSUBID dev \$INTERFACE table stuttgart
+ip route add 10.190.0.0/15 dev \$INTERFACE
+ip route add 10.190.0.0/15 dev \$INTERFACE table stuttgart
+ip addr add fd21:b4dc:4b00::a39:$GWLID$GWLSUBID/40 dev \$INTERFACE metric 512
+ip route add fd21:b4dc:4b00::/40 dev \$INTERFACE metric 512 table stuttgart
+EOF
+chmod +x /etc/tinc/ffsl3/tinc-up
+cat <<EOF >/etc/tinc/ffsl3/tinc-down
+#!/bin/sh
+ip rule del iif \$INTERFACE table stuttgart priority 7000
+EOF
+chmod +x /etc/tinc/ffsl3/tinc-down
+# hosts Datei anpassen nur einmal
+if [ ! -e /etc/tinc/ffsl3/rsa_key.priv ]; then
+  ensureline "address = $HOSTNAME.freifunk-stuttgart.de" /etc/tinc/ffsl3/hosts/$HOSTNAME
+  ensureline "port = 6552" /etc/tinc/ffsl3/hosts/$HOSTNAME
+fi
+# debug auf 2
+cat <<EOF >/etc/default/tinc
+# Debuglevel auf 2
+EXTRA="-d 2"
+EOF
+# tinc aktivieren
+ensureline "ffsl3" /etc/tinc/nets.boot
+}
+
+setup_tincl3_key() {
+  if [ ! -e /etc/tinc/ffsl3/rsa_key.priv ]; then
+    echo | tincd -n ffsl3 -K 4096
+  fi
+# key verschieben
+if [ ! -d /etc/tinc/ffsl3/hosts.1 ]; then
+  mv /etc/tinc/ffsl3/hosts /etc/tinc/ffsl3/hosts.1
+  ln -s /root/tinc-ffsl3/hosts /etc/tinc/ffsl3/hosts
 fi
 }
 
