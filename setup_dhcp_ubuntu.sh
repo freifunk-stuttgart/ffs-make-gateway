3958d5024d2217661fabd6bc35829f9f1b660c7e
albi <albi@seth4.albi.life.de>
2017-03-21 22:23:19 +0100

    dhcpd Anpassungen

--- setup_dhcp_ubuntu.sh
+++ setup_dhcp_ubuntu.sh
@@ -19,57 +19,42 @@ stop=$(($start + 3 ))
 
 cat <<-EOF >/etc/dhcp/dhcpd.conf
 ddns-update-style none;
-default-lease-time 600;
-max-lease-time 600;
+
+default-lease-time 300;
+max-lease-time 300;
 ping-check false;
+
+#log-facility local7;
 log-facility local6;
 option space freifunk;
 option freifunk-encapsulation code 82 = encapsulate freifunk;
-option freifunk.server-id code 11 = { unsigned integer 8, unsigned integer 8, unsigned integer 8, uns
+option freifunk.server-id code 11 = { unsigned integer 8, unsigned integer 8, unsigned integer 8, unsigned integer 8, unsigned integer 8 };
 
-subnet 10.191.255.0 netmask 255.255.255.0 {}
-
-subnet 172.21.0.0 netmask 255.255.192.0 {
-    authoritative;
-    pool
-    {
-     range 172.21.$start.2 172.21.$stop.254;
-     allow all clients;
-    }
-    if (packet(24, 4) != 00:00:00:00) {
-        option routers = packet(24, 4);
-        option freifunk.server-id = packet(24, 4);
-        option domain-name-servers = packet(24, 4);
-        option dhcp-server-identifier = packet(24, 4);
-    }
-    option ntp-servers 172.21.$start.1, 172.21.36.1;
+#option ntp-servers 10.191.255.81, 10.191.255.82, 10.191.255.52;
+authoritative;
+if (packet(24, 4) != 00:00:00:00) {
+    option routers = packet(24, 4);
+    option freifunk.server-id = packet(24, 4);
+    option domain-name-servers = packet(24, 4);
+    option dhcp-server-identifier = packet(24, 4);
+    option ntp-servers = packet(24, 4);
 }
 
+subnet 10.191.255.0 netmask 255.255.255.0 {}
 EOF
-# Segmente durcharbeiten
+
+cat <<-EOF >>/etc/dhcp/dhcpd.conf
+subnet 172.21.0.0 netmask 255.255.192.0 { pool { range 172.21.28.11 172.21.31.254; allow all clients; } }
+EOF
+
 for seg in $SEGMENTLIST ; do
 netz=$((${seg#0} - 1))
 netz=$(($netz * 8))
 start=$(($DHCPID * 2 + $netz ))
 stop=$(($start + 1 ))
-
 cat <<-EOF >>/etc/dhcp/dhcpd.conf
-subnet 10.190.$netz.0 netmask 255.255.248.0 {
-    authoritative;
-    pool
-    {
-     range 10.190.$start.0 10.190.$stop.249;
-     allow all clients;
-    }
-    if (packet(24, 4) != 00:00:00:00) {
-        option routers = packet(24, 4);
-        option freifunk.server-id = packet(24, 4);
-        option domain-name-servers = packet(24, 4);
-        option dhcp-server-identifier = packet(24, 4);
-    }
-    option ntp-servers 10.191.255.11, 10.191.255.81;
-}
-
+subnet 10.190.$netz.0 netmask 255.255.248.0 { pool { range 10.190.$start.0 10.190.$stop.254; allow all clients; } }
 EOF
 done
+
 }
