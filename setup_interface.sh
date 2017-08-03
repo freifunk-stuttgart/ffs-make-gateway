e3b546b6a2aebbc26e5f1b1dc658bc96e7afcb55
root <support@lihas.de>
2017-08-03 13:43:53 +0200

    revert batNN-ip to brNN-ip to enable direct clent interfaces

--- setup_interface.sh
+++ setup_interface.sh
@@ -63,29 +63,44 @@ for seg in $SEGMENTLIST ; do
 netz=$((${seg#0} - 1))
 netz=$(($netz * 8))
 cat <<-EOF >/etc/network/interfaces.d/br$seg
-	auto bat$seg
-	allow-hotplug bat$seg
-	
-	iface bat$seg inet static
-	  hwaddress 02:00:39:$seg:$GWLID:$GWLSUBID
+	auto br$seg
+	iface br00 inet static
+	  bridge_hw 02:00:39:$seg:$GWLID:$GWLSUBID
 	  address 10.190.$netz.$GWID$GWSUBID
 	  netmask 255.255.248.0
+	  bridge_ports bat$seg
+	  bridge_fd 0
+	  bridge_maxwait 0
+	  # be sure all incoming traffic is handled by the appropriate rt_table
+	  post-up         /sbin/ip rule add iif \$IFACE table stuttgart priority 10000 || true
+	  pre-down        /sbin/ip rule del iif \$IFACE table stuttgart priority 10000 || true
+	  post-up         /sbin/ip rule add iif \$IFACE table nodefault priority 10001 || true
+	  pre-down        /sbin/ip rule del iif \$IFACE table nodefault priority 10001 || true
+	  # default route is unreachable
+	  post-up         /sbin/ip route add 172.21.0.0/18 dev \$IFACE table stuttgart || true
+	  post-up         /sbin/ip route add unreachable default table nodefault || true
+	  post-down       /sbin/ip route del unreachable default table nodefault || true
+	  post-down       /sbin/ip route del 172.21.0.0/18 dev \$IFACE table stuttgart || true
+	  auto bat$seg
+	  allow-hotplug bat$seg
+	  post-up         /sbin/ip route add unreachable default table stuttgart metric 9999 || true
+	  post-down       /sbin/ip route del unreachable default table stuttgart metric 9999 || true
+
+	iface br00 inet6 static
+	  address fd21:b4dc:4b$seg::a38:$GWLID$GWLSUBID
+	  netmask 64
+	  # ULA route mz for rt_table stuttgart
+	  post-up         /sbin/ip -6 route add fd21:b4dc:4b$seg::/64 proto static dev \$IFACE table stuttgart || true
+	  post-down       /sbin/ip -6 route del fd21:b4dc:4b$seg::/64 proto static dev \$IFACE table stuttgart || true
+	
+	allow-hotplug bat$seg
+	iface bat$seg inet6 manual
 	  pre-up          /sbin/modprobe batman-adv || true
 	  post-up         /usr/sbin/batctl -m \$IFACE it 10000 || true
 	  post-up         /usr/sbin/batctl -m \$IFACE gw server  64mbit/64mbit || true
 	  # be sure all incoming traffic is handled by the appropriate rt_table
 	  post-up         /sbin/ip rule add iif \$IFACE table stuttgart priority 7000 || true
 	  pre-down        /sbin/ip rule del iif \$IFACE table stuttgart priority 7000 || true
-	  # default route is unreachable. Nur einmal
-	  post-up         /sbin/ip route add unreachable default table stuttgart metric 9999 || true
-	  post-down       /sbin/ip route del unreachable default table stuttgart metric 9999 || true
-	
-	iface bat$seg inet6 static
-	  address fd21:b4dc:4b$seg::a38:$GWLID$GWLSUBID
-	  netmask 64
-	  # ULA route mz for rt_table stuttgart
-	  post-up         /sbin/ip -6 route add fd21:b4dc:4b$seg::/64 proto static dev \$IFACE table stuttgart || true
-	  post-down       /sbin/ip -6 route del fd21:b4dc:4b$seg::/64 proto static dev \$IFACE table stuttgart || true
 	
 	allow-hotplug vpn$seg
 	iface vpn$seg inet6 manual
