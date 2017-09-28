72da2b8be27153b3cf75c09a2671d98d13c6dd6c
albi <albi@seth4.albi.life.de>
2017-09-28 22:46:41 +0200

    irq buffer vergrößert

--- setup_system_ubuntu.sh
+++ setup_system_ubuntu.sh
@@ -4,6 +4,7 @@ net.ipv4.ip_forward=1
 net.ipv6.conf.all.forwarding=1
 net.ipv4.conf.default.rp_filter=0
 net.ipv4.conf.all.rp_filter=0
+net.core.netdev_budget=1000
 EOF
 sysctl -p /etc/sysctl.d/999-freifunk.conf || true
 }
