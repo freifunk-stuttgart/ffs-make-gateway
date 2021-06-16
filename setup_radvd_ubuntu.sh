edbb70e85ca30042701cea467e1d6d9d64a06fb0
root <root@gw09n03.nepustil.net>
2021-06-16 20:32:21 +0000

    add basic ip6 support

--- setup_radvd_ubuntu.sh
+++ setup_radvd_ubuntu.sh
@@ -9,10 +9,18 @@ interface br$seg
   MinRtrAdvInterval 60;
   MaxRtrAdvInterval 300;
   MinDelayBetweenRAs 30;
-  AdvDefaultLifetime 0;
   prefix fd21:b4dc:4b$seg::/64 {};
   RDNSS fd21:b4dc:4b$seg::a38:$GWLID$GWLSUBID {};
   route fd21:b4dc:4b00::/40 {};
+  prefix $IP6$(printf %02x $seg)/64
+  {
+      AdvOnLink on;
+      AdvAutonomous on;
+      AdvRouterAddr on;
+      AdvValidLifetime 7200;
+      AdvPreferredLifetime 300;
+  };
+
 #    prefix ${HE_IPS_V6_PRE}:4b${seg}::/64
 #    {
 #    AdvOnLink on;
